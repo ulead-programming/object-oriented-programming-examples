e41212b7e2336de84253f8af1c237c14a0c5ba8f
Pedro Guzman <pedro@subvertic.com>
2020-11-06 19:24:35 -0600

    Basic implementation of door class

---
+++ src/Door.java
@@ -0,0 +1,49 @@
+public class Door {
+
+    // Class attributes
+    private boolean isClosed;  // If the door is closed this will be True, otherwise it is False
+    private boolean isLocked;
+    private String key;
+
+    public boolean getIsLocked(){
+        return this.isLocked;
+    }
+
+    /**
+     * This is a private method because it is only used within the class and it is not
+     * part of the public interface of the class.
+     * @param key
+     * @return
+     */
+    private boolean keyIsValid(String key){
+        return this.key.compareTo(key) == 0;
+    }
+
+    /**
+     *
+     * @param key
+     * @return
+     */
+    public boolean unlock(String key){
+        boolean successfullyUnlockedTheDoor = false;
+        if(this.isLocked && this.keyIsValid(key)){
+            this.isLocked = false;
+            successfullyUnlockedTheDoor = true;
+        } // IF ENDS
+        return successfullyUnlockedTheDoor;
+    } // METHOD UNLOCK ENDS
+
+    /**
+     *
+     * @return
+     */
+    public boolean open(){
+        boolean successfullyOpenedDoor = false;
+        if(!this.isLocked){
+            this.isClosed = false; // Change the state of the door to open
+            successfullyOpenedDoor = true;
+        }
+        return successfullyOpenedDoor;
+    } // METHOD OPEN ENDS
+
+}
